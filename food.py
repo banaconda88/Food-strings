32eeceae7dc42c7ad3ff40b535be30e3a3a17f28
banaconda88 <bananaconda88@gmail.com>
2021-02-26 21:57:19 -0500

    Add files via upload

---
+++ food.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+A = "I had cinnamon rolls for breakfast."
+print(A)
+B = A.replace("cinnamon rolls", "spicy ramen")
+print(B.replace("breakfast", "lunch"))
+
